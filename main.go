e9d9ce1525bbad1a0d0440ccd9585863bbcaa3b3
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-09-23 04:42:59 +0100

    Updated the port to connect from an environment variable

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"os"
 	"v1/listee/database"
 	"v1/listee/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 
 func main(){
+	godotenv.Load()
 	database.ConnnectDB()
 	router := gin.Default()
 	
 	routes.SetupRoutes(router)
-	router.Run("localhost:9798")
+	router.Run(os.Getenv("PORT"))
 }
